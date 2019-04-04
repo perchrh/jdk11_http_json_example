7600b81b0a2cd6aa8b1c73095cadc421ef2eb81f
Per Henden <per.christian.henden@eika.no>
2019-04-04 12:15:37 +0200

    Comment and remove unnecessary dependencies

--- backend/src/main/java/module-info.java
+++ backend/src/main/java/module-info.java
@@ -1,14 +1,15 @@
 module org.digitalsprouts.example.backend{
     requires java.net.http;
-    requires jersey.server;
     requires java.ws.rs;
     requires java.xml.bind;
-    requires javax.servlet.api;
-    requires hk2.api;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
     requires com.fasterxml.jackson.datatype.jsr310;
-    requires jakarta.inject;
+
+    requires jersey.server; // jersey http-server
+    requires javax.servlet.api; // jersey dependency
+    requires hk2.api; // jersey dependency
+    requires jakarta.inject; // jersey dependency
 
     exports org.digitalsprouts.example.backend.service;
     exports org.digitalsprouts.example.backend.api;
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,14 +1,10 @@
 module http.client.demo.main {
     requires java.net.http;
-    requires jersey.server;
     requires java.ws.rs;
     requires java.xml.bind;
-    requires javax.servlet.api;
-    requires hk2.api;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
     requires com.fasterxml.jackson.datatype.jsr310;
-    requires jakarta.inject;
 
     requires org.digitalsprouts.example.backend;
 }
