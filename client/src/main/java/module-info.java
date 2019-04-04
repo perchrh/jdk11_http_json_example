683184d61819ab5395a014bcb884865eb9548215
Per Henden <per.christian.henden@eika.no>
2019-04-04 12:28:38 +0200

    Add missing requires statements

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module http.client.demo.main {
     requires java.net.http;
     requires java.ws.rs;
     requires java.xml.bind;
+    requires jersey.server;
+    requires hk2.api;
+    requires jakarta.inject;
+
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
     requires com.fasterxml.jackson.datatype.jsr310;
