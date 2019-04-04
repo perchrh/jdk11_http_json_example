d342655d17c2ade7e7e905c1727f977c354b3a2e
Per Henden <per.christian.henden@eika.no>
2019-04-04 12:35:44 +0200

    Prettify a bit

--- client/src/test/java/org/digitalsprouts/example/http_client/ApiClient.java
+++ client/src/test/java/org/digitalsprouts/example/http_client/ApiClient.java
@@ -13,16 +13,20 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+
+import static java.net.http.HttpClient.Version.*;
 
 public class ApiClient {
 
     private final java.net.http.HttpClient client = java.net.http.HttpClient.newBuilder()
-            .version(java.net.http.HttpClient.Version.HTTP_2)
+            .version(HTTP_2)
             .connectTimeout(Duration.ofSeconds(2))
             .build();
 
     private final ObjectMapper objectMapper = Application.createObjectMapper();
+    private final JsonFactory jsonFactory = new JsonFactory();
 
     private final URI baseUri;
 
@@ -32,8 +36,7 @@ public class ApiClient {
 
     private HttpRequest.BodyPublisher createBody(Object dummyRequest) throws IOException {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        JsonFactory jfactory = new JsonFactory();
-        JsonGenerator jGenerator = jfactory.createGenerator(stream, JsonEncoding.UTF8);
+        JsonGenerator jGenerator = jsonFactory.createGenerator(stream, JsonEncoding.UTF8);
 
         objectMapper.writeValue(jGenerator, dummyRequest);
         return HttpRequest.BodyPublishers.ofByteArray(stream.toByteArray());
@@ -43,6 +46,7 @@ public class ApiClient {
         HttpRequest.Builder builder = HttpRequest.newBuilder();
         HttpRequest httpRequest = builder
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT_CHARSET, StandardCharsets.UTF_8.name())
                 .uri(baseUri.resolve(resourcePath))
                 .POST(createBody(data))
                 .build();
